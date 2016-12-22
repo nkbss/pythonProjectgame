b768eba26d17e415a7f9bd01baa5c781ee65f414
nkbss <kanoktat-b@hotmail.com>
2016-12-22 17:53:04 +0700

    random choco and set player

--- project.py
+++ project.py
@@ -1,36 +1,10 @@
 import arcade
 import random
 
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 600
-SPRITE_SCALING = 0.5
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+SPRITE_SCALING = 3
 
-
-class Player(arcade.Sprite):
-    def __init__(self):
-        self.x = 200
-        self.y = 200
-
-    def update(self,dir):
-        self.dir = dir
-        if dir == 0:
-            self.y +=20
-        elif dir == 1:
-            self.y -=20
-        elif dir == 2:
-            self.x += 20
-        elif dir == 3:
-            self.x -= 20
-
-    def on_key_press(key, key_modifiers):
-        if key == arcade.key.UP:
-            self.update(0)
-        elif key == arcade.key.DOWN:
-            self.update(1)
-        elif key == arcade.key.RIGHT:
-            self.update(2)
-        elif key == arcade.key.LEFT:
-            self.update(3)
 
 
 class myProject(arcade.Window):
@@ -38,25 +12,51 @@ class myProject(arcade.Window):
         super().__init__(width, height)
 
         #set up list
-        self.player_sprite = arcade.Sprite("images/chinjung.png")
-        self.choco_sprite = arcade.Sprite("images/ballBlue.png")
+        self.all_sprites_list = None
+        self.choco_list = None
         self.score = 0
+        self.player_sprite = None
 
         arcade.set_background_color(arcade.color.BLACK)
 
+    def start_game(self):
+        #setup lists
+        self.all_sprites_list = arcade.SpriteList()
+        self.choco_list = arcade.SpriteList()
+        #setup player
+        self.player_sprite = arcade.Sprite("chinjung.jpg", SPRITE_SCALING)
+        self.player_sprite.center_x = 200
+        self.player_sprite.center_y = 200
+        self.all_sprites_list.append(self.player_sprite)
 
-    def player_on_key_press(self,key, key_modifiers):
-        self.player.on_key_press(key, key_modifiers)
+        for i in range(20):
+            choco = arcade.Sprite("choco.png",SPRITE_SCALING/3)
+
+            choco.center_x = random.randrange(SCREEN_WIDTH)
+            choco.center_y = random.randrange(SCREEN_HEIGHT)
+
+            self.all_sprites_list.append(choco)
+            self.choco_list.append(choco)
+
+        self.set_mouse_visible(False)
+
 
     def on_draw(self):
         arcade.start_render()
-        self.player_sprite.draw()
+        self.all_sprites_list.draw()
         output = "Score: {}".format(self.score)
         arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
 
     def animate(self,delta):
         self.score = 10
 
+    def on_mouse_motion(self,x, y, dx, dy):
+        self.player_sprite.center_x = x
+        self.player_sprite.center_y = y
+
+
+
 if __name__ == '__main__':
     window = myProject(SCREEN_WIDTH, SCREEN_HEIGHT)
+    window.start_game()
     arcade.run()
