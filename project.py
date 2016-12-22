bd1abd3e0d7e07e66770cc2ad4e5e7393794b2e3
nkbss <kanoktat-b@hotmail.com>
2016-12-22 18:53:55 +0700

    can kill if collision

--- project.py
+++ project.py
@@ -3,16 +3,19 @@ import random
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
-SPRITE_SCALING = 3
+SPRITE_SCALING = 2
 
 class Choco(arcade.Sprite):
-    def reset_pos(self):
-        self.center_y = random.randrange(SCREEN_HEIGHT + 100,
+        def reset_pos(self):
+            self.center_y = random.randrange(SCREEN_HEIGHT + 100,
                                          SCREEN_HEIGHT + 100)
-        self.center_x = random.randrange(SCREEN_WIDTH)
-    def update(self):
-        if self.top < 0:
-            self.reset_pos()
+            self.center_x = random.randrange(SCREEN_WIDTH)
+        def update(self):
+            self.center_y -= 1
+            if self.top < 0:
+                self.reset_pos()
+        def animate(self,delta):
+            self.update()
 
 class myProject(arcade.Window):
     def __init__(self, width, height):
@@ -31,16 +34,16 @@ class myProject(arcade.Window):
         self.all_sprites_list = arcade.SpriteList()
         self.choco_list = arcade.SpriteList()
         #setup player
-        self.player_sprite = arcade.Sprite("chinjung.jpg", SPRITE_SCALING)
+        self.player_sprite = arcade.Sprite("chinjung.jpg")
         self.player_sprite.center_x = 200
         self.player_sprite.center_y = 200
         self.all_sprites_list.append(self.player_sprite)
 
-        for i in range(20):
-            choco = arcade.Sprite("choco.png",SPRITE_SCALING/3)
+        for i in range(2):
+            choco = Choco("choco.png")
 
             choco.center_x = random.randrange(SCREEN_WIDTH)
-            choco.center_y = random.randrange(SCREEN_HEIGHT)
+            choco.center_y = 750
 
             self.all_sprites_list.append(choco)
             self.choco_list.append(choco)
@@ -54,8 +57,21 @@ class myProject(arcade.Window):
         output = "Score: {}".format(self.score)
         arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
 
-    def animate(self,delta):
-        self.score = 10
+    def animate(self,delta_time):
+        self.all_sprites_list.update()
+        for choco in self.choco_list:
+
+            hit_list = arcade.check_for_collision_with_list(self.player_sprite,
+                                                 self.choco_list)
+
+            for choco in hit_list:
+                choco.kill()
+                choco.reset_pos
+                self.score += 1
+
+            if choco.bottom > SCREEN_HEIGHT:
+                choco.kill()
+
 
     def on_mouse_motion(self,x, y, dx, dy):
         self.player_sprite.center_x = x
